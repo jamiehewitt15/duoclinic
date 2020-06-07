a516b91fd1200e8f6a99f6e8d1852bece40ec888
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-06-07 23:20:41 +0300

    adding layout component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Layout from "./components/layout"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>,
   document.getElementById('root')
 );
