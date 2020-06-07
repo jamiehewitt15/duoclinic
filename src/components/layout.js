925189ab0397f9a2cf0ddbe5e782354da5317be8
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-06-07 23:44:17 +0300

    adding initial styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,16 @@
 import React from "react"
+import Header from "./header"
+import "./styles/layout.css"
+
+
 export default function Layout({ children }) {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-      {children}
-    </div>
+      <div>
+          <Header />
+        <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+        {children}
+        </div>
+      </div>
+    
   )
 }
