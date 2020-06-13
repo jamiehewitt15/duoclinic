9e09b57d27a2501522c57eb34fac787a16cb369a
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-06-13 12:00:16 +0300

    creating hero and trial components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hero from "./components/hero.js"
+import Numbers from "./components/numbers.js"
 
 function App() {
   return (
     <div className="App">
+      <Hero />
+      <Numbers />
       
     </div>
   );
