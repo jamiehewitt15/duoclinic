bfa6a01b295f30a8d031c541e998777cabbd9788
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-06-14 21:17:10 +0300

    adding typing effect

---
+++ src/components/featured.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./componentStyles.css"
+
+export default function Featured() {
+      return (    
+<section class="featured light bb96">
+<div class="heading">
+<h2>As featured in</h2>
+</div>
+<ul class="slider slidehome">       
+    <div class="slideshow">
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo1.svg" class="logo1" /></li>
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo3.svg" class="logo3" /></li>
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo4.svg" class="logo4" /></li>
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo5.svg" class="logo5" /></li>
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo6.svg" class="logo6" /></li>
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo7.svg" class="logo7" /></li>
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo8.svg" class="logo8" /></li>
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo9.svg" class="logo9" /></li>
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo10.svg" class="logo10" /></li>
+        <li><img src="https://w10.fit/wp-content/themes/fury-w10/svg/logo11.svg" class="logo11" /></li> 
+    </div>
+</ul>       
+</section>
+      );
+    }
