0edcfd941b51334e2f4aa827481fad0b2deff8d7
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-06-21 12:07:18 +0300

    react router working

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Hero from "./hero.js"
-import Numbers from "./numbers.js"
-import Trial from "./trial.js"
-import Video from "./videos.js"
-import FindUs from "./findUs.js"
+import Hero from "../components/hero.jsx"
+import Numbers from "../components/numbers.jsx"
+import Trial from "../components/trial.jsx"
+import Video from "../components/videos.jsx"
+import FindUs from "../components/findUs.jsx"
 //import Featured from "./components/featured.js"
 
 
