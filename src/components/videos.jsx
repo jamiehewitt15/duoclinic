eb0ace0bd0278700d4c9319a6ec2ff7810653c85
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-07-04 13:10:55 +0300

    mobile optimisation of video
    Former-commit-id: d194200fcd8394c9318c401251eedcef5bddd9e9

--- src/components/videos.jsx
+++ src/components/videos.jsx
@@ -10,11 +10,11 @@ export default function Video() {
         <div className="video">
             <div className="player">
               <div className="lines">
-                <div className="wistia_responsive_padding" style={{padding: '50% 0 0 0', position: 'relative'}}>
-                    <div className="wistia_responsive_wrapper" style={{height: '100%', left: 0, position: 'absolute', top: 0, width: '100%'}}>
+                <div className="wistia_responsive_padding" >
+                    <div className="wistia_responsive_wrapper" >
                         <div className="wistia_embed wistia_async_zsf8r28522 videoFoam=true" style={{height: '100%', width: '100%'}}>
                         <div className="videoContainer">
-                        <iframe title="video1" className="iframeVideo" width="896" height="504" src="https://player.vimeo.com/video/421936046?title=0&byline=0&portrait=0" frameBorder={0} allow="autoplay; fullscreen" allowFullScreen />
+                        <iframe title="video1" className="iframeVideo" src="https://player.vimeo.com/video/421936046?title=0&byline=0&portrait=0" frameBorder={0} allow="autoplay; fullscreen" allowFullScreen />
                         </div><div>
 
                         </div>
