2d8e4a6ad4ce95d6b179695b3925bb20cd8d3d23
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-07-05 19:22:45 +0300

    adding sidebar links
    Former-commit-id: 0940ea721b7c2ab5f89a5b4c00fe4532144c4bf0

--- src/components/sideNav.jsx
+++ src/components/sideNav.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import {  Link } from "react-router-dom";
 import Sidebar from "react-sidebar";
 import "./componentStyles.css";
+import Menu from "./images/menuWhite.svg";
  
 class App extends React.Component {
   constructor(props) {
@@ -18,13 +20,25 @@ class App extends React.Component {
   render() {
     return (
       <Sidebar
-        sidebar={<b>Sidebar content</b>}
+        sidebar={
+        <div className="sideBarMenu">
+          <Link id="covid-link" to="/covid">Our Covid-19 plan</Link>
+          <Link className="sideLink" to="/contact"><p>Contact Us</p></Link>
+          
+        </div>
+          
+        
+        }
         open={this.state.sidebarOpen}
         onSetOpen={this.onSetSidebarOpen}
-        styles={{ sidebar: { background: "white" } }}
+        styles={{ sidebar: { 
+          background: "white",
+          paddingTop: "30px",
+          height: "100vh",
+          position: "fixed", } }}
       >
         <button className="sideBarBtn" onClick={() => this.onSetSidebarOpen(true)}>
-          
+          <img className="menuIcon" src={Menu} alt="Menu Icon"/>
         </button>
       </Sidebar>
     );
