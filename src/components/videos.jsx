5d84b052429e12ed7f9b0759fd3640b8385c206a
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-07-06 10:50:10 +0300

    fixing iframe video
    Former-commit-id: c30f1d40127badcb8dfab203f4a8bcad46c35041

--- src/components/videos.jsx
+++ src/components/videos.jsx
@@ -6,6 +6,8 @@ export default function Video() {
       <div className="holder">
         <div className="findOutMoreHeading">
           <h2 className="specialHeading blue">Find Out More</h2>
+          <iframe title="video1" className="iframeVideo" src='https://player.vimeo.com/video/421936046?title=0&byline=0&portrait=0' frameBorder={0} allow="autoplay; fullscreen" allowFullScreen />
+                        
         </div>
         <div className="video">
             <div className="player">
