94c5e39b7f42858cfac4fa20339bd437168ce074
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-08-04 18:13:19 +0300

    style updates for Our Team section

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -9,26 +9,27 @@ export default function Team() {
     return (
         <section className="teamSection">
           <h2 className="specialHeading blue">Our Team</h2>
-          <div className="table">
+          <div className="table team">
             <div className="tr">
             <div className="td">
+                <div className="no desktop tablet title">Chloe Gibson</div>
               <img className="headshot"  src={image1} alt="Duo Clinic gym trainer headshot" />
-                  <div className="no desktop tablet title">Chloe Gibson</div>
                   <h4><div className="mobile no">Chloe Gibson</div></h4>
             </div>
             <div className="td">
+            <div className="no desktop tablet title">Rob Blackwell</div>
               <img className="headshot" src={image2} alt="Duo Clinic gym trainer headshot" />
-                  <div className="no desktop tablet title">Rob Blackwell</div>
+                  
                   <h4><div className="mobile no">Rob Blackwell</div></h4>
             </div>
             <div className="td">
-              <img className="headshot" src={image3} alt="Duo Clinic gym trainer headshot" />
                   <div className="no desktop tablet title">Jodie Doyle</div>
+              <img className="headshot" src={image3} alt="Duo Clinic gym trainer headshot" />
                   <h4><div className="mobile no">Jodie Doyle</div></h4>
             </div>
             <div className="td">
-              <img className="headshot" src={image4} alt="Duo Clinic gym trainer headshot" />
                   <div className="no desktop tablet title">Neville Kenwright</div>
+              <img className="headshot" src={image4} alt="Duo Clinic gym trainer headshot" />
                   <h4><div className="mobile no">Neville Kenwright</div></h4>
             </div>
             </div>
